67541b92c4a0d4cdd56162e94b4c4bb4c94e8544
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2018-03-15 17:42:59 +0100

    - Minor impro

--- daemonprocessor/src/main/java/com/daemonize/daemonprocessor/SideQuest.java
+++ daemonprocessor/src/main/java/com/daemonize/daemonprocessor/SideQuest.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
 public @interface SideQuest {
-    int SLEEP() default 0;
+    int SLEEP() default 10;
 }
