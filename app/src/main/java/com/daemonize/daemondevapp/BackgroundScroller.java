7bac38faed634730e24983a42092c0672343cd4a
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2018-05-17 10:38:41 +0200

    - image mover impro

--- app/src/main/java/com/daemonize/daemondevapp/BackgroundScroller.java
+++ app/src/main/java/com/daemonize/daemondevapp/BackgroundScroller.java
@@ -11,15 +11,23 @@ public class BackgroundScroller {
 
     private ImageMoverDaemon target;
 
+    private Pair<Integer, Integer> lastTargetCoord = Pair.create(0, 0);
+
     public BackgroundScroller(ImageMoverDaemon target) {
         this.target = target;
     }
 
     @SideQuest(SLEEP = 30)
     public Pair<Integer, Integer> scroll(){
-        return Pair.create(
+        Pair<Integer, Integer> currentTargetCoord = Pair.create(
                 Math.round(target.getLastCoordinates().first + 100),
                 Math.round(target.getLastCoordinates().second + 100)
         );
-    };
+
+        if (currentTargetCoord.equals(lastTargetCoord))
+            return null;
+
+        return currentTargetCoord;
+
+    }
 }
