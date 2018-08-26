4b0481ea9136435a8873c8d80e28a29de07eaf1c
MarkoKuca <marepekovic@gmail.com>
2018-08-26 16:10:01 +0200

    - GenericNode introduced

---
+++ app/src/main/java/com/daemonize/daemondevapp/GenericNode.java
@@ -0,0 +1,57 @@
+package com.daemonize.daemondevapp;
+
+import com.daemonize.daemonengine.closure.Closure;
+import com.daemonize.daemonengine.closure.Return;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenericNode<T> {
+
+    private T value;
+    private List<GenericNode<T>> children;
+    private String name;
+
+    public GenericNode<T> setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public List<GenericNode<T>> getChildren() {
+        return children;
+    }
+
+    public GenericNode(T value) {
+        this.value = value;
+    }
+
+    public GenericNode(T value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public boolean addChild(GenericNode<T> child) {
+        if (children == null) {
+            children = new LinkedList<>();
+        }
+        return children.add(child);
+    }
+
+    public static <K> void forEach(GenericNode<K> root, Closure<K> action) {
+        Return<K> actionReturn = new Return<>(root.getValue());
+        action.onReturn(actionReturn);
+        if(root.getChildren() != null) {
+            for (GenericNode<K> child : root.getChildren()) {
+                forEach(child, action);
+            }
+        }
+    }
+}
