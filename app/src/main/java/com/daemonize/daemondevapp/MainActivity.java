2c6610bd0d4ca46fcdd9f46e192a926be0b1d4b3
marko.pekovic <marko.pekovic@dominionvoting.com>
2018-11-26 18:00:33 +0100

    laser image

--- app/src/main/java/com/daemonize/daemondevapp/MainActivity.java
+++ app/src/main/java/com/daemonize/daemondevapp/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
         //renderer.setWindowSize(borderX, borderY);
         setContentView(renderer);
 
-        int rows = 9;
-        int columns = 13;
+        int rows = 6;
+        int columns = 9;
 
         int width = 160;
         int height = 160;
