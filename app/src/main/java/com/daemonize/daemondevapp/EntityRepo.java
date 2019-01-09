7cd0cb8e9cf50fbed8077e3c5c5b75dd3d64af1e
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2019-01-09 10:09:31 +0100

    - Tower defense merge

--- app/src/main/java/com/daemonize/daemondevapp/EntityRepo.java
+++ app/src/main/java/com/daemonize/daemondevapp/EntityRepo.java
@@ -1,13 +1,10 @@
 package com.daemonize.daemondevapp;
 
-import com.daemonize.daemonengine.Daemon;
-
-import java.util.List;
 
 public interface EntityRepo<T> {
 
     @FunctionalInterface
-    public static interface EntityConfigurator<T> {
+    interface EntityConfigurator<T> {
         void configure(T entity);
     }
 
