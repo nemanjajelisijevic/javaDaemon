b89aeddd28330477ed0955414d1f339f1cf7491c
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2019-01-19 13:07:06 +0100

    Revert "- rendering image null check removed"
    This reverts commit c474a3f

--- app/src/main/java/com/daemonize/daemondevapp/renderer/Renderer2D.java
+++ app/src/main/java/com/daemonize/daemondevapp/renderer/Renderer2D.java
@@ -1,11 +1,10 @@
 package com.daemonize.daemondevapp.renderer;
 
 import com.daemonize.daemondevapp.scene.Scene2D;
-import com.daemonize.daemonengine.consumer.Consumer;
 
-public interface Renderer2D<T extends Renderer2D> extends Consumer {
-    void start();
-    void stop();
+public interface Renderer2D<T extends Renderer2D>{
+    T start();
+    T stop();
     Scene2D getScene();
     T setScene(Scene2D scene);
     void setDirty();
