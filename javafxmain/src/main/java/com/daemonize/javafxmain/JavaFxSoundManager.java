460206c0d345f3769c57c0ff2f906664b23393ec
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2019-07-03 15:26:44 +0200

    - sound manager introduced

--- javafxmain/src/main/java/com/daemonize/javafxmain/JavaFxSoundManager.java
+++ javafxmain/src/main/java/com/daemonize/javafxmain/JavaFxSoundManager.java
@@ -3,17 +3,19 @@ package com.daemonize.javafxmain;
 import com.daemonize.game.soundmanager.SoundManager;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+
 public class JavaFxSoundManager implements SoundManager {
 
     @Override
     public File loadFile(String name) throws URISyntaxException {
-        return new File(getClass().getResourceAsStream("/" + name).toString());
+        return new File(getClass().getResource("/" + name).getPath());
     }
 
     @Override
