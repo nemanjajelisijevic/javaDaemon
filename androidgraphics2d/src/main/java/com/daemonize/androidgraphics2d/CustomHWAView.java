e9dbb4bbdda848ed0a78c6c549d062bdd175eb7b
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2019-10-07 16:45:40 +0200

    - trying out new renderers for hardware acceleration

--- androidgraphics2d/src/main/java/com/daemonize/androidgraphics2d/CustomHWAView.java
+++ androidgraphics2d/src/main/java/com/daemonize/androidgraphics2d/CustomHWAView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.daemonize.daemonengine.utils.DaemonUtils;
 
 public class CustomHWAView extends View {
 
@@ -12,7 +13,6 @@ public class CustomHWAView extends View {
 
     public CustomHWAView(Context context) {
         super(context);
-        setWillNotDraw(false);
     }
 
     CustomHWAView setRenderer(CustomHWAViewRenderer renderer) {
@@ -22,7 +22,7 @@ public class CustomHWAView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (renderer != null && renderer.getDirty())
+        if (renderer.getDirty())
             renderer.drawSceneFromView(canvas);
     }
 }
