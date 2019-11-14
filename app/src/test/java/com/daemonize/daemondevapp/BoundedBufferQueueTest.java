c5c01414295276052d7df4b01ba2185004dcbb74
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2019-11-14 12:01:28 +0100

    - Bounded buffer queue for Renderer

--- app/src/test/java/com/daemonize/daemondevapp/BoundedBufferQueueTest.java
+++ app/src/test/java/com/daemonize/daemondevapp/BoundedBufferQueueTest.java
@@ -12,7 +12,8 @@ import java.util.Queue;
 
 public class BoundedBufferQueueTest {
 
-    private int expectedsSize = 500;
+    private int expectedsSize = 800;
+    private int queueCapacity = 79;
     private List<Integer> expecteds = new ArrayList<>(expectedsSize);
     private BoundedBufferQueue<Integer> queue;
 
@@ -20,16 +21,16 @@ public class BoundedBufferQueueTest {
     public void setUp() {
 
         for (int i = 0; i < expectedsSize; ++i) {
-            expecteds.add(i);
+            expecteds.add(i * 7);
         }
 
-         queue = new BoundedBufferQueue<>(20);
+         queue = new BoundedBufferQueue<>(queueCapacity);
     }
 
     @Test
     public void testEndure() {
 
-        int expectedPolled = 0;
+        int expectedCnt = 0;
         List<Integer> polls = new ArrayList<>(expectedsSize);
 
         for (int i = 0; i < expecteds.size(); ++i) {
@@ -46,9 +47,9 @@ public class BoundedBufferQueueTest {
                 Integer polled = queue.poll();
                 System.out.println("Polled: " + polled);
                 polls.add(polled);
-                queue.add(i);
-                Assert.assertEquals(Integer.valueOf(expectedPolled++), polled);
-                Assert.assertEquals(20, queue.size());
+                queue.add(expecteds.get(i));
+                Assert.assertEquals(Integer.valueOf(expecteds.get(expectedCnt++)), polled);
+                Assert.assertEquals(queueCapacity, queue.size());
                 System.out.println("************************************************");
             }
 
@@ -60,21 +61,29 @@ public class BoundedBufferQueueTest {
 
         System.out.println("DEPLETING QUEUE");
 
-        Integer pollingExpected = expecteds.size() - 20;
+        Integer pollingExpected = expecteds.size() - queueCapacity;
 
         while (!queue.isEmpty()) {
             Integer polled = queue.poll();
             polls.add(polled);
-            Assert.assertEquals(pollingExpected++, polled);
-            System.out.println("Queue state: " + queue.toString());
+            Assert.assertEquals(expecteds.get(pollingExpected++), polled);
+            //System.out.println("Queue state: " + queue.toString());
         }
 
         Assert.assertNull(queue.poll());
-
         Assert.assertEquals(polls.size(), expectedsSize);
 
+        for (int k = 0; k < expecteds.size(); ++k) {
+            Assert.assertEquals(expecteds.get(k), polls.get(k));
+        }
+
+        System.out.println("Expecteds:\n");
+        System.out.println(expecteds.toString());
         System.out.println("Polls:\n");
         System.out.println(polls.toString());
+
+        queue.clear();
+        Assert.assertEquals(0, queue.size());
 
         System.out.println("=========================================================");
     }
