dbaa8ef1e01b3cb111eaedd5ac5cd860f0f1911b
nemanja.jelisijevic <nemanja.jelisijevic@dominionvoting.com>
2019-11-15 12:02:22 +0100

    - benchmark test added for bounded queue

--- app/src/test/java/com/daemonize/daemondevapp/BoundedBufferQueueTest.java
+++ app/src/test/java/com/daemonize/daemondevapp/BoundedBufferQueueTest.java
@@ -1,6 +1,8 @@
 package com.daemonize.daemondevapp;
 
 import com.daemonize.daemonengine.consumer.BoundedBufferQueue;
+import com.daemonize.daemonengine.utils.DaemonUtils;
+import com.daemonize.daemonengine.utils.TimeUnits;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -82,5 +84,58 @@ public class BoundedBufferQueueTest {
         Assert.assertEquals(0, queue.size());
 
         System.out.println("=========================================================");
+    }
+
+    @Test
+    public void bench() {
+
+        System.out.println("No of elements: " + expectedsSize);
+
+        int noOfReps = 100;
+
+        System.out.println("No of repetitions: " + noOfReps);
+
+        long tFills = 0;
+        long tDeps = 0;
+        long tTots = 0;
+
+        for (int t = 0; t < noOfReps; ++t) {
+
+            long t0 = System.nanoTime();
+
+            int expectedCnt = 0;
+            List<Integer> polls = new ArrayList<>(expectedsSize);
+
+            for (int i = 0; i < expecteds.size(); ++i) {
+
+                if (i >= queueCapacity) {
+                    Integer polled = queue.poll();
+                    polls.add(polled);
+                }
+
+                queue.add(expecteds.get(i));
+            }
+
+            long t1 = System.nanoTime();
+
+            while (!queue.isEmpty()) {
+                Integer polled = queue.poll();
+                polls.add(polled);
+            }
+
+            queue.clear();
+
+            long t2 = System.nanoTime();
+
+            tFills += (t1 - t0);
+            tDeps += (t2 - t1);
+            tTots += (t2 - t0);
+
+        }
+
+        System.out.println("Filling avg time: " + DaemonUtils.convertNanoTimeUnits( tFills / noOfReps, TimeUnits.MILLISECONDS));
+        System.out.println("Depleting avg time: " + DaemonUtils.convertNanoTimeUnits( tDeps/ noOfReps, TimeUnits.MILLISECONDS));
+        System.out.println("Totals avg time: " + DaemonUtils.convertNanoTimeUnits( tTots / noOfReps, TimeUnits.MILLISECONDS));
+
     }
 }
